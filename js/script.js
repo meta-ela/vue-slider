affe9cbc54100754867405775e19cbca1bb76ac0
meta-ela <anxhelameta@live.it>
2022-06-16 16:31:52 +0200

    add link vue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+/* 
+Descrizione:
+Far funzionare lo slider partendo dall’html ricevuto.
+Al click su una freccia, l’immagine mostrata cambia.
+la lista delle thumbs deve essere generata tramite un v-for
+
+
+Bonus:
+1- al click su una thumb, visualizzare in grande l’immagine corrispondente
+2 - Al click sulle frecce, cambia l’immagine, ma si evidenzia 
+    anche la thumbnail corrispondente
+3- applicare l’autoplay allo slider: ogni 3 secondi, 
+    cambia immagine automaticamente (vi servirà l’evento mounted() di vue)
+4- quando il mouse va in hover sullo slider, bloccare l’autoplay 
+    e farlo riprendere quando esce
+
+*/
+
 const slides = [
     {
         image: 'img/01.jpg',
@@ -27,3 +45,16 @@ const slides = [
 ];
 
 console.log(slides);
+
+
+const appVue = new Vue({
+    el: "#app",
+
+    data: {
+        boxSlides: slides,
+    },
+
+    methods: {
+
+    },
+});
